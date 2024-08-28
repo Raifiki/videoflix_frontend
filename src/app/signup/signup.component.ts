22e3f42a6dd64eeb69849b0d7ce29121d2ec0335
Raifikie <leonard_weiss@web.dem>
2024-08-28 21:45:54 +0200

    add sign up HTML and css and form control

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+// import custom components
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    FormsModule
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  @ViewChild('iptPwd') iptElementPwd!: ElementRef;
+  @ViewChild('iptPwdConfirm') iptElementPwdCon!: ElementRef;
+
+  email: string = '';
+  password: string = '';
+  passwordConfirm: string = '';
+
+  onSubmit(form: NgForm) {
+    if(this.isPasswordMatching()){
+      console.log('submit', form.value);
+      // ToDo: Send Email and Password to Server and create new User
+    }
+  }
+
+  toggleShowPassword(){
+    if (this.iptElementPwd.nativeElement.type === 'password')
+      this.iptElementPwd.nativeElement.type = 'text';
+    else
+      this.iptElementPwd.nativeElement.type = 'password';
+  }
+
+  toggleShowPasswordConfirm(){
+    if (this.iptElementPwdCon.nativeElement.type === 'password')
+      this.iptElementPwdCon.nativeElement.type = 'text';
+    else
+      this.iptElementPwdCon.nativeElement.type = 'password';
+  }
+
+  isPasswordMatching(): boolean {
+    return this.password === this.passwordConfirm && this.password !== '';
+  }
 
 }
