b4fc3321a34a35f14639d4acf4f9abb13953e7c2
Raifikie <leonard_weiss@web.dem>
2024-08-29 10:55:13 +0200

    add forgot password html and css and navigation to login page

--- src/app/forgotpassword/forgotpassword.component.ts
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+// import custom components
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-forgotpassword',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    FormsModule,
+  ],
   templateUrl: './forgotpassword.component.html',
   styleUrl: './forgotpassword.component.scss'
 })
 export class ForgotpasswordComponent {
+
+
+  onSubmit(form: NgForm) {
+    console.log('submitted');
+  }
 
 }
