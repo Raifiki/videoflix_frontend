da4e2fcc89bb6aae82e47ed442cf0e671ceff07e
Raifikie <leonard_weiss@web.dem>
2024-10-16 16:18:02 +0200

    change backend url to server url

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const ENVIRONEMENT = {
     production: false,
-    backendUrl: 'http://127.0.0.1:8000/videoflix/v1'
+    //backendUrl: 'http://127.0.0.1:8000/videoflix/v1' // localhost
+    backendUrl: 'https://leonard-weiss.developerakademie.org/videoflix/v1' // production
 }
